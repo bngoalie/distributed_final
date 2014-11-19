034727067024d93149a6c42df6214d89015b4557
Ben <bngoalie@gmail.com>
2014-11-18 23:09:07 -0500

    updated headers, wrote out some pseduocode for server

--- server.h
+++ server.h
@@ -1,3 +1,5 @@
+#ifndef CHAT_SERVER_H
+#define
 /*
  * server.h
  *
@@ -19,31 +21,61 @@ typedef struct {
 typedef struct {
     char[MAX_LINE_LENGTH] message;
 } append_payload;
+#define APPEND_PAYLOAD_SIZE sizeof(append_payload)
 
 typedef struct {
     int toggle; // 0 for unlike, 1 for like
     lamport_timestamp lts; // We index messages by lts, not a global line number (at least for now).
 } like_payload;
+#define LIKE_PAYLOAD_SIZE sizeof(like_payload)
+
 
 typedef struct {
-    char[MAX_CHAT_ROOM_LENGTH] chat_room;
+    int toggle; // 0 for leave, 1 for join
 } join_payload;
+#define JOIN_PAYLOAD_SIZE sizeof(join_payload)
+
 
 typedef struct {
     int type;
     lamport_timestamp lts;
-    char[MAX_USERNAME_LENGTH] username;
+    char username[MAX_USERNAME_LENGTH];
+    char chat_room;[MAX_CHAT_ROOM_LENGTH];
     update_payload payload;
 } update;
 
+#define UPDATE_SIZE_WITHOUT_PAYLOAD = sizeof(update)-sizeof(update_payload)
 
 typedef struct update_node {
     update update;
-    update_node *next;
+    struct update_node *next;
 } update_node;
-/*
-typedef struct room_node {
-    char[MAX_CHAT_ROOM_LENGTH] chat_room;
-    char
 
-}*/
+typedef struct client_node {
+    char client_group[MAX_GROUP_NAME];  // TODO: Replace with join update? Would be for use with other server's clients
+    update_node *join_update;
+    struct client_node *next;
+} client_node;
+
+typedef struct liker_node {
+    update_node *like_update_node;
+    struct liker_node *next;
+} liker_node;
+
+typedef struct line_node {
+    update_node *append_update_node;
+    liker_node likers_list_head; // TODO: consider keeping this list sorted, so could use a tail pointer to quickly check if the username already is in list.
+    struct line_node *next;   
+}
+
+typedef struct room_node {
+    char chat_room[MAX_CHAT_ROOM_LENGTH];
+    /* TODO: consider char[] for spread group for chat room isntead of recomputing it everytime want to send message to clients*/
+    struct room_node *next;
+    /* TODO: Determine if a tail pointer would be beneficial. */
+    client_node my_clients_head;
+    client_node other_clients_head;
+    line_node lines_list_head;
+    line_node *twenty_fifth_oldest_line; // Consider instead implementing a doubly-linked list so can count from end? 
+} room_node;
+#endif
---
+++ support.h
@@ -0,0 +1,5 @@
+/* returns -1 if lts1->lts2, 1 if lts2->lts1, 0 otherwise (non-causally dependent)*/
+int compare_lts(lamport_timestamp lts1, lamport_timestamp lts2);
+
+
+void get_room_group(int server_id, char *room, char *room);
