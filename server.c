fc5e32950074e2d3276eb8f86512299b078aa6ec
Ben <bngoalie@gmail.com>
2014-11-26 13:47:22 -0500

    starting more server code

--- server.c
+++ server.c
@@ -13,8 +13,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "support.h"
+#include "server.h"
 
-void main(int argc, char *argv[]){
+#define MAX_MEMBERS     10
+#define MAX_GROUPS      MAX_MEMBERS
+
+char	    User[80];
+char        Spread_name[80];
+char        Private_group[MAX_GROUP_NAME];
+char        group[MAX_GROUP_NAME];
+mailbox     Mbox;
+int	        Num_sent;
+struct      timeval start_time;
+struct      timeval end_time, end_time_last_send, end_time_last_receive;
+int         num_processes;
+int         process_index;
+int         seq;
+FILE        *fd = NULL;
+
+
+room_node room_list_head; // Should I make this the lobby?
+update_node update_list_head;
+
+/* TODO: Intent is to keep traack of updates received from each server
+ * could be simply replaced with an int array for seqs, but need to update only when receive an 
+ * update that is one higher than current. if receive an even higher seq, need to remember that received this seq.
+ * This could evolve into some kind of sliding window mechnism liken that used in ex1 and ex2*/
+update_node *most_recent_server_updates[5]; // used for checking most recent seq from each server? TODO: replace 5 with macro. 
+
+void main(int argc, char *argv[]) {
+
+    /* Set up list of rooms (set up the lobby) */
+    room_list_head.twenty_fifth_oldest_line = NULL;
+    room_list_head.next = NULL;
+
+    /* Set up list of updates */
+    update_list_head.next = NULL;
     
+    /* TODO: Read last known state from disk*/
+
+
 
 }
--- support.h
+++ support.h
@@ -1,3 +1,7 @@
+#define SPREAD_SERVER_GROUP "server_group"
+
+
+
 /* returns -1 if lts1->lts2, 1 if lts2->lts1, 0 otherwise (non-causally dependent)*/
 int compare_lts(lamport_timestamp lts1, lamport_timestamp lts2);
 
